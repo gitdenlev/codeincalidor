3b7fcbd2ebec6ddb7cd41abafd5fbd5a3f20c87b
Lisa Riabokon <lisa.checheleva@gmail.com>
2022-12-13 17:42:40 +0200

    add test slider

--- src/js/slick.js
+++ src/js/slick.js
@@ -15,10 +15,53 @@ $('.variable-width').slick({
   variableWidth: true,
 });
 
-$('.hp-restaurant-slider').slick({
-  arrows: true,
-  slidesToShow: 2,
-  variableWidth: true,
-  appendArrows: $('.restaurant-details__slider-nav'),
-  speed: 300,
+// $('.hp-restaurant-slider').slick({
+//   dots: false,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 2,
+//   adaptiveHeight: true,
+//   appendArrows: $('.hp-restaurant-slider__buttons'),
+//   variableWidth: true,
+// });
+
+$(document).ready(function () {
+  $('.restaurant-details-slider').slick({
+    arrows: true,
+    slidesToShow: 2,
+    variableWidth: true,
+    appendArrows: $('.restaurant-details__slider-nav'),
+    speed: 300,
+  });
 });
+
+// $(document).ready(function () {
+//   $('.slider').slick({
+//     arrows: true,
+//     adaptiveHeight: true,
+//     slidesToShow: 2,
+//     slidesToScroll: 1,
+//     speed: 300,
+//     autoplay: true,
+//     autoplaySpeed: 5000,
+//     pauseOnFocus: true,
+//     pauseOnHover: true,
+//     variableWidth: true,
+//     responsive: [
+//       {
+//         breakpoint: 1024,
+//         settings: {
+//           slidesToShow: 3,
+//         },
+//       },
+//       {
+//         breakpoint: 1280,
+//         settings: {
+//           slidesToShow: 3,
+//         },
+//       },
+//     ],
+//     mobileFirst: true,
+//     appendArrows: $('.hp-restaurant-slider__buttons'),
+//   });
+// });
