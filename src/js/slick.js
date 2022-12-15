faead6ed37710043f18ff0c5e32b8dcab9c5d7bd
Bodia98 <stefanyuk1998@gmail.com>
2022-12-15 19:05:18 +0200

    js

--- src/js/slick.js
+++ src/js/slick.js
@@ -38,10 +38,10 @@ $('.fade').slick({
   cssEase: 'linear',
 });
 
-$('.one-time-slick').slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 2,
-  adaptiveHeight: true,
-});
+// $('.one-time-slick').slick({
+//   dots: false,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 2,
+//   adaptiveHeight: true,
+// });
