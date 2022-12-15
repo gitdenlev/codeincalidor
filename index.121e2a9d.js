da23126d8ce4ff12acf9525267c8e5bc3019ed28
gitdenlev <113287842+gitdenlev@users.noreply.github.com>
2022-12-15 10:27:05 +0000

    Deploying to gh-pages from @ gitdenlev/codeincalidor@5a029e1c770fe524aec5f066e1d71677ff32d6de 🚀

--- index.121e2a9d.js
+++
@@ -1,2 +0,0 @@
-$(".one-time").slick({dots:!1,infinite:!0,speed:300,slidesToShow:2,adaptiveHeight:!0}),$(".variable-width").slick({dots:!1,infinite:!0,speed:300,slidesToShow:1,centerMode:!0,variableWidth:!0}),$(".fade").slick({dots:!0,infinite:!0,speed:600,fade:!0,autoplay:!0,cssEase:"linear"});
-//# sourceMappingURL=index.121e2a9d.js.map
