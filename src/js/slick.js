61aa7b3d058aaa65b0cc86989f10927949b8f4c0
Lisa Riabokon <lisa.checheleva@gmail.com>
2022-12-15 02:25:57 +0200

    add slider breakpoints

--- src/js/slick.js
+++ src/js/slick.js
@@ -15,28 +15,10 @@ $('.variable-width').slick({
   variableWidth: true,
 });
 
-// $('.hp-restaurant-slider').slick({
-//   dots: false,
-//   infinite: true,
-//   speed: 300,
-//   slidesToShow: 2,
-//   adaptiveHeight: true,
-//   appendArrows: $('.hp-restaurant-slider__buttons'),
-//   variableWidth: true,
-// });
-
-// $(document).ready(function () {
-//   $('.slider').slick();
-// });
-
-// $(document).ready(function () {
-//   $('.slider').slick();
-// });
-
-$('.one-time').slick({
-  dots: false,
-  infinite: true,
+$('.slider').slick({
+  arrows: true,
+  slidesToShow: 2,
+  variableWidth: true,
+  // appendArrows: $('.arrows'),
   speed: 300,
-  slidesToShow: 1,
-  adaptiveHeight: true,
 });
