4a9f1ae7b65a1d3be3de5bd68d8ae0f7519134c8
Vladislav <vladislavkukochka@gmail.com>
2022-12-17 14:21:45 +0200

    first-modal-table/kukochka

--- src/js/table-modal.js
+++ src/js/table-modal.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openTableModalBtn: document.querySelector("[data-table-modal-open]"),
-    closeTableModallBtn: document.querySelector("[data-table-modal-close]"),
-    tableModal: document.querySelector("[data-table-modal]"),
+    openTableModalBtn: document.querySelector('[data-table-modal-open]'),
+    closeTableModalBtn: document.querySelector('[data-table-modal-close]'),
+    tableModal: document.querySelector('[data-table-modal]'),
   };
 
-  refs.openTableModalBtn.addEventListener("click", toggleTableModal);
-  refs.closeTableModalBtn.addEventListener("click", toggleTableModal);
+  refs.openTableModalBtn.addEventListener('click', toggleTableModal);
+  refs.closeTableModalBtn.addEventListener('click', toggleTableModal);
 
   function toggleTableModal() {
-    refs.tableModal.classList.toggle("is-hidden");
+    refs.tableModal.classList.toggle('is-hidden');
   }
 })();
